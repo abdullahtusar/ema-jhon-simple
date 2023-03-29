38fe1e5121b2e87613d5f63654fc9c03b4ce78c9
abdullahtusar <abdullahtusar2@gmail.com>
2023-03-29 19:26:56 +0600

    full shopping cart calculation

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,20 +4,26 @@ const Cart = ({cart}) => {
     console.log(cart);
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        totalPrice = totalPrice + product.price;
+        // if(product.quantity === 0){
+        //     product.quantity = 1;
+        // }
+        //product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice*7/100;
     const grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='cart'>
             <h3>Order Summary</h3>
-            <p>Selected Item: {cart.length}</p>
+            <p>Selected Item: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
-            <h3>Grand Total: {grandTotal.toFixed(2)}</h3>
+            <h3>Grand Total: ${grandTotal.toFixed(2)}</h3>
         </div>
     );
 };
